54f7e66a9780edf18930fe1eb5deb028799bf893
Daimon-J <daimon.java.best@gmail.com>
2022-01-26 08:20:01 -0800

    1. Что такое итератор. [#4951]

--- src/main/java/ru/job4j/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -6,7 +6,7 @@ import java.util.NoSuchElementException;
 public class BackwardArrayIt implements Iterator<Integer> {
 
     private final int[] date;
-    private int point = 3;
+    private int point = 2_147_483_647;
 
     public BackwardArrayIt(int[] data){
         this.date = data;
